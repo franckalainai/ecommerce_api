50405cdce57d8f1a979e46eb41d2ae100792ce08
franckalainai <franckalain.ai@gmail.com>
2021-07-02 14:27:03 +0000

    implicit model binding avec routes et models

--- app/Buyer.php
+++ app/Buyer.php
@@ -2,8 +2,16 @@
 
 namespace App;
 
+use App\Scopes\BuyerScope;
+
 class Buyer extends User
 {
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new BuyerScope);
+    }
+
     // un buyer a beaucoup de transactions
     public function transactions()
     {
