a7ae61dfc260a39e932e2bced40a163b75fbde53
franckalainai <franckalain.ai@gmail.com>
2021-07-08 11:07:02 +0000

    implementations des operations complexes du buyer

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
 Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
 Route::resource('buyers.products', 'Buyer\BuyerProductController', ['only' => ['index']]);
+Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => ['index']]);
+Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' => ['index']]);
 
 
 // prendre en compte toutes les methodes sauf create et edit
