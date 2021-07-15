ed2e583bfd9bfcc2e23e8b7d028c995d3025929e
franckalainai <franckalain.ai@gmail.com>
2021-07-15 12:04:17 +0000

    sorting and filtering base on query parameters

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -18,27 +18,51 @@ trait ApiResponser
         return response()->json(['error' => $message, 'code' => $code], $code);
     }
 
-    public function showAll(Collection $collection, $code = 200)
+    protected function showAll(Collection $collection, $code = 200)
     {
         if ($collection->isEmpty()) {
             return $this->successResponse(['data' => $collection], $code);
         }
 
         $transformer = $collection->first()->transformer;
+
+        $collection = $this->filterData($collection, $transformer);
+        $collection = $this->sortData($collection, $transformer);
         $collection = $this->transformData($collection, $transformer);
+
         return $this->successResponse($collection, $code);
     }
 
-    public function showOne(Model $instance, $code = 200)
+    protected function showOne(Model $instance, $code = 200)
     {
         $transformer = $instance->transformer;
         $instance = $this->transformData($instance, $transformer);
         return $this->successResponse($instance, $code);
     }
 
-    public function showMessage($message, $code = 200)
+    protected function showMessage($message, $code = 200)
     {
         return $this->successResponse(['data' => $message], $code);
+    }
+
+    protected function filterData(Collection $collection, $transformer)
+    {
+        foreach (request()->query() as $query => $value) {
+            $attribute = $transformer::originalAttribute($query);
+            if (isset($attribute, $value)) {
+                $collection = $collection->where($attribute, $value);
+            }
+        }
+        return $collection;
+    }
+
+    protected function sortData(Collection $collection, $transformer)
+    {
+        if (request()->has('sort_by')) {
+            $attribute = $transformer::originalAttribute(request()->sort_by);
+            $collection = $collection->sortBy->{$attribute};
+        }
+        return $collection;
     }
 
     protected function transformData($data, $transformer)
